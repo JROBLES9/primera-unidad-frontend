14dd5e30e3614d7d3fc4546b5037271f0058b6ea
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-10 21:55:55 -0600

    add component page Prueba

---
+++ src/views/Prueba/Prueba.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+const PruebaComponent = () => {
+  return (
+    <div>
+      <h1>Prueba</h1>
+      <p>Esto es una prueba</p>
+    </div>
+  );
+}
+
+export default PruebaComponent;
