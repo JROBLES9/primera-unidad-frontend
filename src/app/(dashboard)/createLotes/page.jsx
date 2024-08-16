6d9639eec980fe2f62c628d882ef184b21e79a94
Keithleen Ivana Ruíz Estrada <125908174+keithre19@users.noreply.github.com>
2024-08-16 07:20:28 -0600

    feat: Add searchParams to createLotes page

--- src/app/(dashboard)/createLotes/page.jsx
+++ src/app/(dashboard)/createLotes/page.jsx
@@ -5,6 +5,17 @@ import FormLayoutsWithIcon from '@views/forms/FormCreateLotes';
 
 const createLotes = () => {
 
+    const searchParams = useSearchParams();
+
+    const idLote = searchParams.get('idLote');
+    const categoria = searchParams.get('categoria');
+    const cantidadInicial = searchParams.get('cantidadInicial');
+    const cantidadDisponible = searchParams.get('cantidadDisponible');
+    const fechaCaducidad = searchParams.get('fechaCaducidad');
+    const fechaIngreso = searchParams.get('fechaIngreso');
+    const idPedido = searchParams.get('idPedido');
+    const idProducto = searchParams.get('idProducto');
+
     return (
         <Grid item xs={12} md={6} width={0.75}
             boxShadow={'4px 4px 20px rgba(94, 1, 110, 0.35)'}
@@ -12,7 +23,16 @@ const createLotes = () => {
             marginTop={'5%'}
             align={'center'}
         >
-            <FormLayoutsWithIcon />
+            <FormLayoutsWithIcon
+                idLote={idLote}
+                categoria={categoria}
+                cantidadInicial={cantidadInicial}
+                cantidadDisponible={cantidadDisponible}
+                fechaCaducidad={fechaCaducidad}
+                fechaIngreso={fechaIngreso}
+                idPedido={idPedido}
+                idProducto={idProducto}
+            />
         </Grid>
     );
 }
