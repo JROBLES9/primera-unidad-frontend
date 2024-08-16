96bfa04cdcf7f6bb6f406dbd969ef0e313828c28
Josué Robles <88120613+JROBLES9@users.noreply.github.com>
2024-08-16 09:16:44 -0600

    feat: Add colaboradores page component

---
+++ src/app/(dashboard)/colaboradores/page.jsx
@@ -0,0 +1,9 @@
+import ColaboradoresComponent from '@views/colaboradores/colaboradores';
+
+const colaboradores = () => {
+    return (
+        <ColaboradoresComponent />
+    );
+}
+
+export default colaboradores;
